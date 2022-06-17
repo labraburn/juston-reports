f51454bd8fbcc146723eae36149de6a28d5da00a
Anton Spivak <oboupo@gmail.com>
2022-06-17 23:04:34 +0300

    Remove comments

--- packages/HuetonCORE/Sources/HuetonCORE/HuetonCORE.swift
+++ packages/HuetonCORE/Sources/HuetonCORE/HuetonCORE.swift
@@ -40,29 +40,3 @@ public struct HuetonCORE {
         ManagedObjectContextObjectsDidChangeObserver.startObservingIfNeccessary()
     }
 }
-
-
-// PROD
-//
-// Balance: ?
-// Address: "EQBKCMGcAoyyG85L3SIakVRLMfwhp7-xA13jTWAYO1jgpb81" // v4r2
-// Words: []
-
-// PROD
-//
-// Balance: ?
-// Address: "EQCMfNwPB8TaNqQ9hnXCYcXOz41jfI5PCawHe1ZvwKfKXTXM" // united
-// Words: []
-
-// PROD
-//
-// Balance: 34
-// Address: EQCd3ASamrfErTV4K6iG5r0o3O_hl7K_9SghU0oELKF-sxDn // v3r2
-// Words: []
-
-// TEST
-//
-// Balance: 14.9
-// Address: EQAVhOY2uT49tcvM6rRJII25bgEqEBWu6ZywXrtaqYtvIlMk
-// Address: EQCIJiFJrN8kuwdXEIfmJ-D7qwP-QfLX8YtCAhaY6AoSKxUv ????????????????????????????????
-// Words: ["episode", "diary", "tower", "either", "void", "into", "until", "universe", "loan", "answer", "own", "ribbon", "adapt", "step", "tuna", "innocent", "accident", "female", "already", "nasty", "wrist", "tenant", "toast", "post"]
